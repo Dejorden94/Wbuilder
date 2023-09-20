e81c97f5f289728929c3ada19ec50d6f639757cb
Dejorden <d.moerman@hotmail.nl>
2023-09-20 17:21:45 +0200

    cards are now renderd based on the numebr of json object

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-{/* <article class="card">
+/* <article class="card">
 <header class="card__header">
     <h1 class="card__heading">Card 1</h1>
 </header>
@@ -12,7 +12,86 @@
         <button class="card__action">Action</button>
     </footer>
 </div>
-</article> */}
+</article> */
 
-fetch("../data/cards.json").then(response => response.json()).then(data => console.log(data));
+const renderCards = (cardsData) => {
+    for (let i = 0; i < cardsData.length; i++) {
+        document.querySelector("body").appendChild(makeCard(cardsData[i]))
+    }
+}
 
+const makeCard = (cardObject) => {
+    let article = document.createElement("article");
+
+    article.classList = "card";
+    article.appendChild(makeHeader(cardObject.title));
+    article.appendChild(makeDiv(cardObject));
+
+    return article;
+}
+
+const makeHeader = (title) => {
+    let header = document.createElement("header");
+    header.classList = "card__header";
+
+    let heading = document.createElement("h1");
+    heading.classList = "card__heading";
+    heading.innerText = title;
+    header.appendChild(heading);
+
+    return header;
+}
+
+const makeDiv = (cardObject) => {
+    let div = document.createElement("div");
+
+    div.appendChild(makeBody(cardObject.text));
+    div.appendChild(makeFooter(cardObject.action))
+
+    return div;
+}
+
+const makeFooter = (buttonText) => {
+    let footer = document.createElement("footer");
+    footer.classList = "card__footer";
+
+    footer.appendChild(makeButton(buttonText));
+
+    return footer;
+}
+
+const makeButton = (actionText) => {
+    let button = document.createElement("button");
+    button.classList = "card__action";
+
+    button.innerText = actionText;
+
+    return button;
+}
+
+const makeBody = (text) => {
+    let section = document.createElement("section");
+    section.classList = "card__body";
+
+    section.appendChild(makeText(text));
+
+    return section;
+}
+
+const makeText = (text) => {
+    let paragraf = document.createElement("p");
+    paragraf.classList = "card__text";
+    paragraf.innerText = text;
+
+    return paragraf;
+}
+
+const init = () => {
+    fetch("../data/cards.json").then(
+        response => response.json()
+    ).then(data => {
+        renderCards(data)
+    })
+};
+
+init();
